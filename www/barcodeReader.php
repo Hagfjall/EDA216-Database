96d61a90f3ce098367257233eb8fef9acefbe8c7
Fredrik Hagfjäll <hagfjall@gmail.com>
2015-03-25 12:20:11 +0100

    Finally solved merge problem

--- www/barcodeReader.php
+++ www/barcodeReader.php
@@ -4,7 +4,7 @@
 	$db = $_SESSION['db'];
 	$palletId  = $_SESSION['scannedPalletId'] = $_REQUEST['freezerExit'];
 	$db->openConnection();
-	$orders = $db->freezerExitScanner($palletId);
+	$orders = $db->getOrdersWithProduct($palletId);
 	$db->closeConnection();
 ?>
 
@@ -16,27 +16,23 @@
 <body>
 	<p>
 		<?php
-		print_r($orders);
-		// print $orders[0];
+		 print $orders[0];
 		?>
 	</p>
-<!--
 <form method=post action="deliverPallet.php">
 		<select name="orderId" size=10>
 		<?php
-			/**$first = true;
+			$first = true;
 
 			foreach ($orders as $order) {
 					print "<option>";
 					print $order['orderId'];
 			}
-			*/
 		?>
 		</select>
 <br>
 		<input type=submit value="Deliver Pallet">
 	</form>
--->
 
 
 
